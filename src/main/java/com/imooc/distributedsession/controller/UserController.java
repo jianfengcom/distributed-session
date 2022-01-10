3e74c622cf77c8d0ce7e4f59086bcc89e0108800
bugger <bugger@email.com>
2022-01-10 12:06:10 +0800

    4-6 Token + Redis @RequestParam 可省去

--- src/main/java/com/imooc/distributedsession/controller/UserController.java
+++ src/main/java/com/imooc/distributedsession/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.imooc.distributedsession.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private static final String LOGIN_USER = "login_user";
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping("/login")
     public String login(String username,
@@ -22,5 +27,18 @@ public class UserController {
     @GetMapping("/info")
     public String login(HttpSession session) {
         return "当前登录的是：" + session.getAttribute(LOGIN_USER);
+    }
+
+    @GetMapping("/token/login")
+    public String loginWithToken(@RequestParam String username,
+                                 @RequestParam String password) {
+        String key = "token_" + UUID.randomUUID().toString();
+        stringRedisTemplate.opsForValue().set(key, username, 1, TimeUnit.HOURS);
+        return key;
+    }
+
+    @GetMapping("/token/info")
+    public String infoWithToken(@RequestHeader String token) {
+        return "当前登录的是：" + stringRedisTemplate.opsForValue().get(token);
     }
 }
