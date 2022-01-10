01cc906595ec0f73960ff73f842004dd14e4d27e
bugger <bugger@email.com>
2022-01-10 11:31:45 +0800

    init 1. maven(无骨架) -> springboot 2. 热部署

---
+++ src/main/java/com/imooc/distributedsession/DistributedSessionApplication.java
@@ -0,0 +1,12 @@
+package com.imooc.distributedsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedSessionApplication.class, args);
+    }
+}
